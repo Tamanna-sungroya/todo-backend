74fa41db4d509d1461923065e2ab79c5c6eb1304
tamanna1223 <sungroyat@gmail.com>
2025-12-01 21:06:01 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config()
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB connected"))
+.catch((err)=>console.log(err))
+
+const Todo = mongoose.model('Todo', new mongoose.Schema({
+    text: String
+}));
+
+app.get("/todos", async (req, res) => {
+    res.json( await Todo.find());
+})
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server started on port ${process.env.PORT || 5000}`);
+});
